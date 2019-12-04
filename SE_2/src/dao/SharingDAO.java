4da5614455bbb210df41d1f0724e4a598b6d8455
una97 <yoona607@dongguk.edu>
2019-12-05 02:04:14 +0900

    checkDuplication

--- SE_2/src/dao/SharingDAO.java
+++ SE_2/src/dao/SharingDAO.java
@@ -1,29 +1,70 @@
 package dao;
+
 import dto.*;
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class SharingDAO{
+public class SharingDAO {
 	private Connection conn;
 	private PreparedStatement pstmt;
 	private ResultSet rs;
 
-	public SharingDAO()
-	{
+	public SharingDAO() {
 		try {
-			String dbURL ="jdbc:mysql://docamp.cgxcxscgnhfb.us-east-2.rds.amazonaws.com:3306/docamp?serverTimezone=UTC";
-			String dbID ="egg";
-			String dbPassword ="12345678";
+			String dbURL = "jdbc:mysql://docamp.cgxcxscgnhfb.us-east-2.rds.amazonaws.com:3306/docamp?serverTimezone=UTC";
+			String dbID = "egg";
+			String dbPassword = "12345678";
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
+	public int checkDuplication( int p_id, String s_start, String s_end) {
+		
+		String sql = "select s_r_start, s_r_end from shar_reservation where s_r_p_id=? ";
+		try {
+		    pstmt = conn.prepareStatement(sql);
+	        pstmt.setInt(1, p_id);
+	        rs= pstmt.executeQuery();
+	        SimpleDateFormat format = new SimpleDateFormat( "yyyy-MM-dd" );
+	        Date myStart,myEnd,dbStart,dbEnd ;
+            int s_s,e_s,s_e,e_e;
+	        String start, end;
+	        
+	        myStart=format.parse(s_start);
+	        myEnd=format.parse(s_end);
 	
-	
-	
-	
-	
-	
-	
+	        
+	        if(rs.next()) {
+	        	start= rs.getString(1);
+	        	end = rs.getString(2);
+	            dbStart = format.parse(start);
+	            dbEnd = format.parse(end);
+	         
+	            s_s = myStart.compareTo(dbStart);
+	            e_s = myEnd.compareTo(dbStart);
+	            s_e = myStart.compareTo(dbEnd);
+	            e_e = myEnd.compareTo(dbEnd);
+	            
+	        	if((s_s<=0 && e_s>0)||(s_e<0 && e_e>=0)) {
+	        		
+	        		return 0; //겹치는 날짜가 있어서 안돼 
+	        	}
+	        	else
+	        		return 1; //겹치지 않아서 성공 
+	        }
+	        return -1; //아무도 이 상품을 신청하지 않음 
+	        
+		}catch(Exception e) {
+	          e.printStackTrace();
+	       }
+		
+	return -2; //database오류 
+		
+	}
+
 }
---
+++ SE_2/src/service/SharingCheck.java
@@ -0,0 +1,73 @@
+package service;
+
+import dao.*;
+import java.io.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SharingCheck extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public SharingCheck() {
+		super();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+
+		int s_p_id = Integer.parseInt(request.getParameter("s_p_id"));
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		String strDate = dateFormat.format(	request.getDateHeader("start"));
+//		
+		String strDate1= request.getParameter("start");
+	
+		
+//		 DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");  
+//         String strDate = dateFormat.format(request.getParameter("start"));  
+//         String endDate = dateFormat.format(request.getParameter("end"));
+////		String startDay = new SimpleDateFormat("yyyy-MM-dd").format(request.getParameter("start"));
+//
+////		String endDay = new SimpleDateFormat("yyyy-MM-dd").format(request.getParameter("end"));
+//
+		PrintWriter script = response.getWriter();
+		script.println("<script>");
+		script.println("alert(" + s_p_id + ")");
+		script.println("alert(" + strDate + ")");
+		script.println("window.close()");
+		script.println("</script>");
+//		SharingDAO shrDAO = new SharingDAO();
+//		int result = shrDAO.checkDuplication(s_p_id, strDate, endDate);
+//
+//		if (result == 0) {
+//			script.println("<script>");
+//			script.println("alert('겹쳐')");
+//			script.println("window.close()");
+//			script.println("</script>");
+//
+//		} else if (result == 1 || result == -1) {
+//			script.println("<script>");
+//			script.println("alert('성')");
+//			script.println("window.close()");
+//			script.println("</script>");
+//
+//		} else {
+//			script.println("<script>");
+//			script.println("alert('오류')");
+//			script.println("window.close()");
+//			script.println("</script>");
+//		}
+
+	}
+
+}
